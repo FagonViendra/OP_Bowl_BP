139d932de8495f3a1aa31c3d4f682399c6f452ff
Fagon <fagon@example.com>
2026-03-20 22:06:48 +0700

    Sửa lỗi Script API không load (1.2.1)

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-import { world, system } from "@minecraft/server";
+import { world, system, EquipmentSlot } from "@minecraft/server";
 
 system.runInterval(() => {
     for (const player of world.getAllPlayers()) {
         const equippable = player.getComponent("minecraft:equippable");
         if (!equippable) continue;
 
-        const mainhand = equippable.getEquipment("Mainhand");
+        const mainhand = equippable.getEquipment(EquipmentSlot.Mainhand);
 
         if (mainhand && mainhand.typeId === "custom:op_bowl") {
             player.addEffect("night_vision", 300, {
